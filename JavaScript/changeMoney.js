168f1d3d3ff7bd4f1a333808d30f1b83b86760b8
Jack Wu <jackwu350@yahoo.com>
2017-07-10 20:54:51 -0700

    Update changeMoney.js

--- JavaScript/changeMoney.js
+++ JavaScript/changeMoney.js
@@ -1,4 +1,14 @@
 
+//Task. The goal in this problem is to find the minimum number of coins needed to change the input value
+//(an integer) into coins with denominations 1, 5, and 10.
+
+//Input Format. The input consists of a single integer 𝑚.
+//Constraints. 1 ≤ 𝑚 ≤ 103.
+
+//Output Format. Output the minimum number of coins with denominations 1, 5, 10 that changes 𝑚.
+
+
+
 var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
@@ -32,10 +42,5 @@ function coinChange(change,numCoins){
 
 
   return coinChange(newChange,numCoins + 1)
-
-
-
-
-
 
 }
