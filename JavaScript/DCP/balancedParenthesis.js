54352bad96892a85ea6174f7c90bd5713b7c2362
Jack Wu <jackwu350@gmail.com>
2018-10-01 15:24:16 -0700

    daily

---
+++ JavaScript/DCP/balancedParenthesis.js
@@ -0,0 +1,33 @@
+Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced.
+
+
+function isBalanced(string) {
+  let stack = [];
+  let hash = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
+
+  for(let char of string) {
+      if (['(', '[', '{'].includes(char)) {
+        stack,push(char);
+      }
+      else if ([')', '}', ']'].includes(char)){
+        if(stack.length === 0){
+          return false;
+        }
+
+        if (hash[stack.pop()] !== char ) {
+          return false;
+        }
+      }
+  }
+
+  if (stack.length > 0) {
+    return false;
+  } else{
+    return true;
+  }
+
+}
